0548bf73c220aaaf3d339c9787efe1364cba04d3
Sohel-Developer <sohel.webdevoloper.com>
2022-11-15 15:33:21 +0600

    Home Page in Banner Feature added

--- src/sharead/Navbar/Navbar.js
+++ src/sharead/Navbar/Navbar.js
@@ -10,7 +10,8 @@ const Navbar = () => {
         <li><Link className='ml-6 p-0 bg-none bg-transparent hover:text-primary font-bold' to={'/contuct'}>Contuct Us</Link></li>
     </>
     return (
-        <div className="navbar bg-base-100">
+
+        <div className="navbar  bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -42,6 +43,7 @@ const Navbar = () => {
                 </div>
             </div>
         </div>
+
     );
 };
 
